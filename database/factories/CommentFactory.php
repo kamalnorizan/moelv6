35bfd08efcb32292f771b25e1d4253d7c7b67d99
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-06-07 14:43:49 +0800

    factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
     return [
-        //
+        'content' => $this->faker->sentence(rand(5,10)),
+        'user_id' => $this->faker->numberBetween(1,200),
+        'post_id' => $this->faker->numberBetween(1,400)
     ];
 });
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'content' => $this->faker->paragraph(rand(5,10)),
+        'user_id' => $this->faker->numberBetween(1,200)
     ];
 });
