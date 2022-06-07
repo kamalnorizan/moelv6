092409da09a921914d569a437d81f89f15a6e2e8
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-06-07 12:40:53 +0800

    models

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    //
+    use SoftDeletes;
+    public $timestamps = true;
+
+    protected $table = 'comments';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $guarded = ['id'];
+
+
 }
--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+    public $timestamps = true;
+
+    protected $table = 'posts';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = true;
+
+    protected $guarded = ['id'];
 
 }
