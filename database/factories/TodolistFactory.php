89d174a733c56c33e837096c714378208ac89a8d
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-06-07 11:30:48 +0800

    init

---
+++ database/factories/TodolistFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Todolist;
+use Faker\Generator as Faker;
+
+$factory->define(Todolist::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/TodolistSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TodolistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
